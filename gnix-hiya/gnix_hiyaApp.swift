60826cb36dc955db7d8336ed299b0ff7cb24799d
davigomesflorencio <davigomesflorencio@gmail.com>
2026-01-27 21:04:03 -0300

    Initial Commit

---
+++ gnix-hiya/gnix_hiyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  gnix_hiyaApp.swift
+//  gnix-hiya
+//
+//  Created by Davi Gomes Florencio on 27/01/26.
+//
+
+import SwiftUI
+
+@main
+struct gnix_hiyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
